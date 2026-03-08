dfdbee429ff256e63500d9c38b0c220e84160390
difmo <difmotech@gmail.com>
2026-03-08 21:12:04 +0530

    save

--- data/api/apiClient.js
+++ data/api/apiClient.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+    // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
 
-    // baseURL: "https://shrimpbite-backend.vercel.app/api",
+    baseURL: "https://shrimpbite-backend.vercel.app/api",
     headers: {
         "Content-Type": "application/json",
     },
