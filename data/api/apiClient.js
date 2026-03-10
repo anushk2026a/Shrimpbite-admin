e4bc27efea8132dbcab84e45b2554aef32789e93
anushk2026a <anushk2026a@gmail.com>
2026-03-10 10:22:41 +0530

    apiclient

--- data/api/apiClient.js
+++ data/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "https://shrimpbite-backend.onrender.com/api",
+    baseURL: "https://shrimpbite-backend.vercel.app/api",
     // baseURL: "http://localhost:5000/api",
     headers: {
         "Content-Type": "application/json",
