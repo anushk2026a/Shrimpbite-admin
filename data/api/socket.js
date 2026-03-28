ca78bf02837f8de2bd31643bf6b80106230467c3
anushk2026a <anushk2026a@gmail.com>
2026-03-28 15:11:33 +0530

    socket

--- data/api/socket.js
+++ data/api/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://shrimpbite-socket-server.onrender.com";
+const SOCKET_URL = "https://api.shrimpbite.in";
 // const SOCKET_URL = "http://localhost:5001";
 
 const socket = io(SOCKET_URL, {
